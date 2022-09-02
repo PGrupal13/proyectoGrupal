3249e3e0c249b31e2f1a4cfe8617f55506bfb299
Alejandro Giraldo <magiraldo2224@gmail.com>
2022-09-02 08:27:58 -0500

    Cambio link dashboard

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -10,6 +10,6 @@ st.set_page_config(
 st.title("Dashboard")
 @st.experimental_memo
 def load_dashboard():
-    return 'https://app.powerbi.com/view?r=eyJrIjoiMDA3YTk0OTktYTE1Ni00Yzc1LTkyYTQtMmYzYWZmMjBhZDdlIiwidCI6IjQ1OGEwZGVkLWVlN2ItNDdjYy04MTg1LTJmM2Q2MDY1YjQ1MCJ9&pageName=ReportSection'
+    return 'https://app.powerbi.com/view?r=eyJrIjoiOTRjNWFmNjctOWE2NS00NWEwLWI0MjEtZjJiYjBiYzM0NGI2IiwidCI6IjQ1OGEwZGVkLWVlN2ItNDdjYy04MTg1LTJmM2Q2MDY1YjQ1MCJ9'
 
 st.components.v1.iframe(load_dashboard(), height=700)
